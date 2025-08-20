907dadb792e3831af08c02b20516de89d46a6a88
Sang Duong <sangdev2109@gmail.com>
2025-08-20 14:41:22 +0700

    add docker, cicd

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+BRANCH=${BRANCH:-develop}
+IMAGE=ghcr.io/sangdv219/cat:$BRANCH
+PORT=${APP_PORT:-3000}
+
+echo "[INFO] Using branch: $BRANCH"
+echo "[INFO] Deploying $APP_NAME with image $IMAGE"
+
+# Stop old container
+docker stop $APP_NAME || true
+docker rm $APP_NAME || true
+
+# Pull latest image
+docker pull $IMAGE
+
+# Run new container
+docker run -d --name $APP_NAME \
+  -p 3000:3000 \
+  $IMAGE
+
+
+  # Healthcheck (optional)
+sleep 5
+if curl -fs http://localhost:$APP_PORT/health >/dev/null; then
+  echo "[INFO] ✅ $APP_NAME is up and healthy on port $APP_PORT"
+else
+  echo "[ERROR] ❌ $APP_NAME failed to start" >&2
+  exit 1
+fi
+
+echo "[INFO] $APP_NAME deployed on port $PORT"
---
+++ scripts/deploy-staging.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+BRANCH=${BRANCH:-develop}
+IMAGE=ghcr.io/sangdv219/cat:$BRANCH
+PORT=3000
+
+echo "[INFO] Using port: $PORT"
+echo "[INFO] Using branch: $BRANCH"
+echo "[INFO] Deploying $APP_NAME with image $IMAGE"
+
+# Stop old container
+docker stop $APP_NAME || true
+docker rm $APP_NAME || true
+
+# Pull latest image
+docker pull $IMAGE
+
+# Run new container
+docker run -d --name $APP_NAME \
+  -p 3000:3000 \
+  $IMAGE
+
+
+  # Healthcheck (optional)
+sleep 10
+if curl -fs http://54.252.231.194:3000 >/dev/null; then
+  echo "[INFO] ✅ $APP_NAME is up and healthy on port $APP_PORT"
+else
+  echo "[ERROR] ❌ $APP_NAME failed to start" >&2
+  exit 1
+fi
+
+echo "[INFO] $APP_NAME deployed on port $PORT"
